c326b0d78b40fb1153cd516f4a279dadd055ae4c
hayden.rear <hayden.rear@gmail.com>
2025-08-20 10:51:13 -0400

    test mcp server

--- src/main/java/com/hayden/mcptoolgateway/fn/RedeployFunction.java
+++ src/main/java/com/hayden/mcptoolgateway/fn/RedeployFunction.java
@@ -1,9 +1,11 @@
 package com.hayden.mcptoolgateway.fn;
 
 import com.hayden.mcptoolgateway.config.ToolGatewayConfigProperties;
+import lombok.Builder;
 
 public interface RedeployFunction {
 
+    @Builder
     record RedeployDescriptor(boolean isSuccess, String err) {
 
     }
--- src/test/java/com/hayden/mcptoolgateway/McpToolGatewayApplicationTests.java
+++ src/test/java/com/hayden/mcptoolgateway/McpToolGatewayApplicationTests.java
@@ -1,14 +1,21 @@
 package com.hayden.mcptoolgateway;
 
+import com.hayden.mcptoolgateway.fn.RedeployFunction;
 import com.hayden.utilitymodule.delegate_mcp.DynamicMcpToolCallbackProvider;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,13 +23,39 @@ class McpToolGatewayApplicationTests {
 
     @Autowired
     DynamicMcpToolCallbackProvider dynamicMcpToolCallbackProvider;
+    @MockitoBean
+    RedeployFunction redeployFunction;
+
+    @BeforeEach
+    public void setupRedeploy() {
+        AtomicInteger i = new AtomicInteger(0);
+        Mockito.when(redeployFunction.performRedeploy(any()))
+                .thenAnswer(invocation -> {
+                    if (i.getAndIncrement() == 0) {
+                        return RedeployFunction.RedeployDescriptor.builder().isSuccess(true).build();
+                    }
+
+                    if (i.get() == 1) {
+
+                    }
+
+                    return RedeployFunction.RedeployDescriptor.builder().isSuccess(true).build();
+                });
+    }
 
     @Test
     void contextLoads() {
     }
 
     @Test
-    void containsTestJar() {
+    void containsTestJarActiveClient() {
+        assertThat(new File("build/libs/test-mcp-server.jar").exists()).isTrue();
+        assertThat(dynamicMcpToolCallbackProvider.containsClient("test-mcp-server")).isTrue();
+        assertThat(dynamicMcpToolCallbackProvider.containsActiveClient("test-mcp-server")).isTrue();
+    }
+
+    @Test
+    void redeployTestMcpServer() {
         assertThat(new File("build/libs/test-mcp-server.jar").exists()).isTrue();
         assertThat(dynamicMcpToolCallbackProvider.containsClient("test-mcp-server")).isTrue();
         assertThat(dynamicMcpToolCallbackProvider.containsActiveClient("test-mcp-server")).isTrue();
