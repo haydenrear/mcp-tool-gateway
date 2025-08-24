5c00f51798615a1a0a1a4122e9fabf38e169bdb2
hayden.rear <hayden.rear@gmail.com>
2025-08-24 11:34:54 -0400

    adding test

--- src/main/java/com/hayden/mcptoolgateway/tool/ToolModels.java
+++ src/main/java/com/hayden/mcptoolgateway/tool/ToolModels.java
@@ -3,5 +3,5 @@ package com.hayden.mcptoolgateway.tool;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public interface ToolModels {
-    record Redeploy(@JsonProperty("Service to redeploy") String deployService) {}
+    record Redeploy(@JsonProperty("service_name") String deployService) {}
 }
