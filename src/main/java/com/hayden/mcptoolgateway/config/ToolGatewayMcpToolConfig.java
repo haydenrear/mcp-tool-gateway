577129c517d50370edd1fa7c8e1747af4a409eb0
hayden.rear <hayden.rear@gmail.com>
2025-10-17 16:48:19 -0400

    disable grapql DisableGraphQl

--- src/main/java/com/hayden/mcptoolgateway/config/ToolGatewayMcpToolConfig.java
+++ src/main/java/com/hayden/mcptoolgateway/config/ToolGatewayMcpToolConfig.java
@@ -1,14 +1,17 @@
 package com.hayden.mcptoolgateway.config;
 
+import com.hayden.commitdiffmodel.config.DisableGraphQl;
 import com.hayden.utilitymodule.delegate_mcp.DynamicMcpToolCallbackProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @Configuration
+@Import(DisableGraphQl.class)
 public class ToolGatewayMcpToolConfig {
 
     @Bean
