7db40712bf41b59fb81cac2a9817ccb22789683a
hayden.rear <hayden.rear@gmail.com>
2025-11-07 16:47:04 -0500

    flush refs more smartly

--- src/main/java/com/hayden/mcptoolgateway/tool/ToolModels.java
+++ src/main/java/com/hayden/mcptoolgateway/tool/ToolModels.java
@@ -3,5 +3,9 @@ package com.hayden.mcptoolgateway.tool;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public interface ToolModels {
+
+
     record Redeploy(@JsonProperty("service_name") String deployService) {}
+
+
 }
